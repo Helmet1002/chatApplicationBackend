3281bff4dac62cb0ceb861f2c7de11a5561fcb7d
Helmet1002 <siddhantsinghrajput246@gmailcom>
2025-03-15 19:17:23 +0530

    initial commit for chatApp project

---
+++ src/main/java/com/chatApp/backend/controller/ChatAppController.java
@@ -0,0 +1,11 @@
+package com.chatApp.backend.controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatAppController {
+
+public void api(){
+}
+}
